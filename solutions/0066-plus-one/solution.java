50da79928f8fee6fb73d79aef912f4c19617ebc2
Atharva Shyam samrat <samrat9473@gmail.com>
2026-02-17 18:36:37 +0000

    Sync LeetCode submission Runtime - 3 ms (1.41%), Memory - 44.1 MB (8.45%)

--- solutions/0066-plus-one/solution.java
+++ solutions/0066-plus-one/solution.java
@@ -1,16 +1,19 @@
 import java.math.BigInteger;
 class Solution {
     public static  int[] plusOne(int[] digits) {
-        for (int i = digits.length - 1; i >= 0; i--) {
-            if (digits[i] < 9) {
-                digits[i] += 1;
-                return digits;
-            }else{
-                digits[i] = 0;
-            }
+        BigInteger num = BigInteger.ZERO;
+        for (int j : digits) {
+            num = num.multiply(BigInteger.TEN);
+            num = num.add(BigInteger.valueOf(j));
         }
-        int[] digit = new int[digits.length+1];
-        digit[0]=1;
+        num= num.add(BigInteger.ONE);
+        
+        String str = num.toString();
+       int[] digit = new int[str.length()];
+
+        for (int i = 0; i < str.length(); i++) {
+        digit[i] = str.charAt(i)-'0';
+}
         return digit;
     }
 }

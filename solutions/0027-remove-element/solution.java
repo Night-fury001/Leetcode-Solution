4d6208bb835a45e3320cd8098e9f8bf4cf256ac0
Atharva Shyam samrat <samrat9473@gmail.com>
2026-03-18 05:31:59 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.2 MB (95.76%)

--- solutions/0027-remove-element/solution.java
+++ solutions/0027-remove-element/solution.java
@@ -3,7 +3,8 @@ class Solution {
         int j = 0;
         for(int i : nums){
             if(i != val){
-                nums[j++] = i;
+                nums[j] = i;
+                j++;
             }
         }
         return j;

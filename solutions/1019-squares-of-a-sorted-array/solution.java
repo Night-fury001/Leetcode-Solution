eb332221b2e6485c85e3debb5f42df8a5bddfaf6
Atharva Shyam samrat <samrat9473@gmail.com>
2026-08-26 16:19:48 +0000

    Sync LeetCode submission Runtime - 1 ms (100.00%), Memory - 47.7 MB (30.23%)

---
+++ solutions/1019-squares-of-a-sorted-array/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int[] result = new int[nums.length];
+        int left = 0;
+        int right = nums.length - 1;
+        int pos = result.length - 1;
+        while(left <= right){
+            if (Math.abs(nums[left]) <= Math.abs(nums[right])){
+                result[pos] = nums[right] * nums[right];
+                right--;
+            }else{
+                result[pos] = nums[left] * nums[left];
+                left ++;
+            }
+            pos--;
+        }
+        return result;
+    }
+}

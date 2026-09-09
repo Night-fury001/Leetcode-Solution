47f67bdaec1721ee487cebcf6f926261cd350d49
Atharva Shyam samrat <samrat9473@gmail.com>
2026-09-09 07:30:42 +0000

    Sync LeetCode submission Runtime - 2 ms (100.00%), Memory - 45.6 MB (99.70%)

---
+++ solutions/0013-roman-to-integer/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int romanToInt(String s) {
+        int total = 0;
+        int pre = 0;
+        for(int i = s.length() - 1; i >= 0; i--){
+            
+            int cur = switch(s.charAt(i)){
+                case 'I' -> 1;
+                case 'V' -> 5;
+                case 'X' -> 10;
+                case 'L' -> 50;
+                case 'C' -> 100;
+                case 'D' -> 500;
+                case 'M' -> 1000;
+                default  -> 0;
+            };
+            if (cur < pre){
+                total -= cur;
+            }else{
+                total += cur;
+            }
+            pre = cur;
+        }
+        return total;
+    }
+}

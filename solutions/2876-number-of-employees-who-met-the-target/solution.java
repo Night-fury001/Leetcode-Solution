cfd55e3e8c68bd8dc0aaf0628d9c4b5d5b26d66a
Atharva Shyam samrat <samrat9473@gmail.com>
2026-09-15 15:54:07 +0000

    Sync LeetCode submission Runtime - 1 ms (22.90%), Memory - 44.2 MB (22.69%)

---
+++ solutions/2876-number-of-employees-who-met-the-target/solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int emp = 0;
+        for (int i : hours){
+            if(i >= target) emp++;
+        }
+        return emp;
+    }
+}

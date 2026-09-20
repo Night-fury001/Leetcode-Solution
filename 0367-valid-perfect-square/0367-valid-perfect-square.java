94b3a64992548198b509f7b07896434ddd00d582
Atharva Shyam samrat <samrat9473@gmail.com>
2026-09-20 15:29:04 +0530

    Time: 0 ms (100%), Space: 42.2 MB (37.91%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        long  n = num;
+        while (n * n > num){
+            n = (n + num / n) / 2;
+        }
+        return n * n == num;
+    }
+}
